44907b4e355c30096e079628b651610197838c26
Fernando Masino <49108644+fom78@users.noreply.github.com>
2022-12-15 11:18:42 -0300

    inicial (#2) (#3)
    * Create base project
    
    * Create initial pokemon application
    
    * Add evolution functionality
    
    Co-authored-by: Enrique Gimeno <enriquejgimeno@gmail.com>
    
    Co-authored-by: Enrique Gimeno <enriquejgimeno@gmail.com>

---
+++ README.old.md
@@ -0,0 +1,30 @@
+# prueba1-proyecto
+* Mártes 15-11-22 (fwarno)  
+La idea es que cada uno arme su branch y trabaje en ella.  
+Para el nombre de la brach, 1ra letra del nombre y a continuacion el apellido, todo en minúsculas.(ej:fwarno)  
+A medida que vamos practicando y resolviendo lo vamos pasando a la main y vemos como resulta.  
+Cuaquier cosa armamos otra organización pero ya vamos a tener algo de código hecho y algo de práctica.  
+Otra forma de trabajar puede ser que cada uno dentro de la organización cree su propio repositorio y trabaje tranquilo en ese repositorio con su código y luego cuando este listo lo puede pasar o copiar a la branch propia del repositorio principal "prueba1-proyecto".  
+De esta forma si alguien arma algo básico y lo quiere ir probando y mostrando al grupo de lo que ya tiene hecho, los demas miembros podrian clonar ese repositorio como opción y verlo en su propia computadora.
+
+# fmasino
+
+Ya dentro ja, como deben realizarlo, por las dudas
+- crear carpeta de trabajo
+```sh
+md carpeta
+```
+- moverse a ella
+```sh
+cd capeta
+```
+- abrir terminal dentro de la carpeta
+- clonan:
+```sh
+git clone https://github.com/22808-Grupo03/prueba1-proyecto.git
+```
+- crear rama para trabajar
+```sh
+git switch -C nombrerama
+```
+
---
+++ src/components/Visualizer.jsx
@@ -0,0 +1,72 @@
+import axios from "axios";
+import React, { useState, useCallback, useEffect } from "react";
+import { Controls } from "./Controls";
+import { Image } from "./Image";
+import "./Visualizer.css";
+
+export const Visualizer = () => {
+  const [currentEvolution, setCurrentEvolution] = useState(0);
+  const [evolutionChainId, setEvolutionChainId] = useState(0);
+  const [pictureUrls, setPictureUrls] = useState([]);
+
+  const getEvolutionChain = useCallback((id) => {
+    axios
+      .get("https://pokeapi.co/api/v2/evolution-chain/" + id + "/")
+      .then((response) => {
+        let evoluciones = response.data.chain.evolves_to;
+        let tieneEvolucion = true;
+        let urls = [];
+        // get basic pokemon picture
+
+        axios.get(response.data.chain.species.url).then((response) => {
+          axios.get(response.data.varieties[0].pokemon.url).then((response) => {
+            urls.push(response.data.sprites.front_default);
+            setPictureUrls(urls);
+            // console.log(pictureUrls);
+          });
+        });
+
+        // get evolutions pictures
+
+        while (tieneEvolucion) {
+          if (evoluciones.length > 0) {
+            axios.get(evoluciones[0].species.url).then((response) => {
+              axios
+                .get(response.data.varieties[0].pokemon.url)
+                .then((response) => {
+                  urls.push(response.data.sprites.front_default);
+                  setPictureUrls(urls);
+                });
+            });
+            evoluciones = evoluciones[0].evolves_to;
+          } else {
+            tieneEvolucion = false;
+          }
+        }
+      });
+  }, []);
+
+  useEffect(() => {
+    if (evolutionChainId > 0) {
+      getEvolutionChain(evolutionChainId);
+    }
+  }, [evolutionChainId, getEvolutionChain]);
+
+  const controlProps = {
+    evolutionChainId,
+    setEvolutionChainId,
+    currentEvolution,
+    setCurrentEvolution,
+    pictureUrls,
+  };
+  const imageProps = {
+    currentEvolution,
+    pictureUrls,
+  };
+  return (
+    <div className="visualizer-container">
+      <Image {...imageProps} />
+      <Controls {...controlProps} />
+    </div>
+  );
+};
