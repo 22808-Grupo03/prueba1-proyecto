8553d1b53bfbc2198e42cb1ddc3931cae4042c61
Fernando Masino <49108644+fom78@users.noreply.github.com>
2022-12-16 14:24:58 -0300

    Adding bootstrap styles (#4)
    Co-authored-by: Fernando Mongelos <fernandomongelos911@gmail.com>

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -3,10 +3,10 @@ import "./Image.css";
 
 export const Image = ({ currentEvolution, pictureUrls }) => {
   return (
-    <div className="image-container">
+    <div className="bg-primary border border-black border-2 p-2 m-2 rounded d-flex justify-content-center img">
       {pictureUrls.length > 0 ? (
         <img
-          className="image"
+          className=""
           src={pictureUrls[currentEvolution]}
           alt="pokemon"
         />
